f54d245135dcc5eeac0749aa9da628a6e9bcbc01
Tom Yeh <tomyeh@zkoss.org>
2011-11-08 16:06:47 +0800

    expression evaluation

---
+++ zuss/src/org/zkoss/zuss/Utils.java
@@ -0,0 +1,61 @@
+/* Utils.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Tue Nov  8 15:51:23 TST 2011, Created by tomyeh
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+*/
+package org.zkoss.zuss;
+
+/**
+ * Utilities for handling CSS values, such as font size, color and so on.
+ *
+ * @author tomyeh
+ */
+public class Utils {
+	/** Negates the given value. */
+	public static Object negate(Object val) {
+		return null;
+	}
+	/** Adds the given values. */
+	public static Object add(Object val0, Object val1) {
+		return null;
+	}
+	/** Subtracts the given values. */
+	public static Object subtract(Object val0, Object val1) {
+		return null;
+	}
+	/** Multiplies the given values. */
+	public static Object multiply(Object val0, Object val1) {
+		return null;
+	}
+	/** Divides the given values. */
+	public static Object divide(Object val0, Object val1) {
+		return null;
+	}
+
+	/** Test if the given values equal to each other. */
+	public static boolean equals(Object val0, Object val1) {
+		return false;
+	}
+	/** Compares the given values.
+	 * @return 1 if val0 is greater, 0 if equals, -1 if smaller
+	 */
+	public static int compare(Object val0, Object val1) {
+		return 0;
+	}
+
+	/** OR two conditions. */
+	public static Object or(Object val0, Object val1) {
+		return null;
+	}
+	/** AND two conditions. */
+	public static Object and(Object val0, Object val1) {
+		return null;
+	}
+}
